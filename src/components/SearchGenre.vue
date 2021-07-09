338a86eb78c3ebdf0c63154f42679d8164607295
Marco Tessitore <marcotexcode@gmail.com>
2021-07-09 13:36:40 +0200

    add creazione form

--- src/components/SearchGenre.vue
+++ src/components/SearchGenre.vue
@@ -1,17 +1,36 @@
 <template>
-  <h3>
-      hello
+  
+    <form>
 
-  </h3>
+        <label for="">Seleziona il genere</label>
+
+        <select name="genre" id="genre">
+
+            <option value="rock">Rock</option>
+
+            <option value="pop">Pop</option>
+
+            <option value="jazz">Jazz</option>
+
+            <option value="metal">Metal</option>
+
+        </select>
+
+    </form>
+
 </template>
 
+
 <script>
+
 export default {
 
     name: 'SearchGenre'
 
 }
+
 </script>
+
 
 <style>
 
